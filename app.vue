a010bab81ccd6f6487eb4cb2604940544de320e2
clifford-devops3 <devops.3@clifford.co.ke>
2023-08-24 18:11:33 +0300

    Initial launch

---
+++ app.vue
@@ -0,0 +1,3 @@
+<template>
+ <h3 class="text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint impedit molestiae ex autem mollitia deleniti perferendis dolor ut ipsum amet tenetur alias quas aperiam pariatur aut, laborum eveniet et fuga.</h3>
+</template>
